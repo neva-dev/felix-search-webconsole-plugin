3ebf5739afe4a23f523333cc61d46cc763b48ecc
krystian.panek <krystian.panek@cognifide.com>
2017-03-01 08:54:26 +0100

    #1 | REST params unified

--- src/main/java/com/neva/felix/webconsole/plugins/search/rest/ClassSearchServlet.java
+++ src/main/java/com/neva/felix/webconsole/plugins/search/rest/ClassSearchServlet.java
@@ -1,12 +1,9 @@
 package com.neva.felix.webconsole.plugins.search.rest;
 
-import com.google.common.collect.Lists;
 import com.neva.felix.webconsole.plugins.search.core.BundleClass;
 import com.neva.felix.webconsole.plugins.search.core.OsgiExplorer;
 import com.neva.felix.webconsole.plugins.search.core.SearchMonitor;
 import com.neva.felix.webconsole.plugins.search.core.classsearch.ClassSearchJob;
-import org.apache.commons.lang3.ArrayUtils;
-import org.apache.commons.lang3.StringUtils;
 import org.osgi.framework.BundleContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,8 +12,6 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.Collections;
-import java.util.List;
 import java.util.Set;
 
 import static com.neva.felix.webconsole.plugins.search.utils.JsonUtils.MessageType;
@@ -27,14 +22,6 @@ public class ClassSearchServlet extends RestServlet {
     private static final Logger LOG = LoggerFactory.getLogger(ClassSearchServlet.class);
 
     public static final String ALIAS_NAME = "class-search";
-
-    private static final String PHRASE_PARAM = "phrase";
-
-    private static final String JOB_ID_PARAM = "jobId";
-
-    private static final String BUNDLE_ID_PROP = "bundleId[]";
-
-    private static final String BUNDLE_CLASS_PROP = "bundleClass[]";
 
     private final SearchMonitor<ClassSearchJob> monitor;
 
@@ -60,8 +47,9 @@ public class ClassSearchServlet extends RestServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         try {
-            final String phrase = getPhrase(request);
-            final Set<BundleClass> classes = osgiExplorer.findClasses(getBundleIds(request), getBundleClasses(request));
+            final RestParams params = RestParams.from(request);
+            final String phrase = params.getPhrase();
+            final Set<BundleClass> classes = osgiExplorer.findClasses(params.getBundleIds(), params.getBundleClasses());
             final ClassSearchJob job = new ClassSearchJob(osgiExplorer, phrase, classes);
 
             monitor.start(job);
@@ -75,7 +63,7 @@ public class ClassSearchServlet extends RestServlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         try {
-            String jobId = getJobId(request);
+            String jobId = RestParams.from(request).getJobId();
             ClassSearchJob job = monitor.get(jobId);
 
             if (job == null) {
@@ -92,7 +80,7 @@ public class ClassSearchServlet extends RestServlet {
 
     protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         try {
-            String jobId = getJobId(request);
+            String jobId = RestParams.from(request).getJobId();
             ClassSearchJob job = monitor.get(jobId);
 
             if (job == null) {
@@ -105,25 +93,6 @@ public class ClassSearchServlet extends RestServlet {
             LOG.error("Cannot search classes.", e);
             writeMessage(response, MessageType.ERROR, "Internal error occurred.");
         }
-    }
-
-    private String getPhrase(HttpServletRequest request) {
-        return StringUtils.trimToEmpty(request.getParameter(PHRASE_PARAM));
-    }
-
-    private String getJobId(HttpServletRequest request) {
-        return StringUtils.trimToEmpty(request.getParameter(JOB_ID_PARAM));
-    }
-
-    private List<String> getBundleIds(HttpServletRequest request) {
-        String[] values = request.getParameterValues(BUNDLE_ID_PROP);
-        return ArrayUtils.isNotEmpty(values) ? Lists.newArrayList(values) : Collections.<String>emptyList();
-    }
-
-    private List<String> getBundleClasses(HttpServletRequest request) {
-        final String[] values = request.getParameterValues(BUNDLE_CLASS_PROP);
-
-        return ArrayUtils.isEmpty(values) ? Collections.<String>emptyList() : Lists.newArrayList(values);
     }
 
 }
---
+++ src/main/java/com/neva/felix/webconsole/plugins/search/rest/RestParams.java
@@ -0,0 +1,53 @@
+package com.neva.felix.webconsole.plugins.search.rest;
+
+import com.google.common.collect.Lists;
+import org.apache.commons.lang3.ArrayUtils;
+import org.apache.commons.lang3.StringUtils;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Place for unified param names used in multiple REST endpoints.
+ */
+public class RestParams {
+
+    private static final String PHRASE_PARAM = "phrase";
+
+    private static final String JOB_ID_PARAM = "jobId";
+
+    private static final String BUNDLE_ID_PROP = "bundleId[]";
+
+    private static final String BUNDLE_CLASS_PROP = "bundleClass[]";
+
+    private final HttpServletRequest request;
+
+    public RestParams(HttpServletRequest request) {
+        this.request = request;
+    }
+
+    public static RestParams from(HttpServletRequest request) {
+        return new RestParams(request);
+    }
+
+    public String getPhrase() {
+        return StringUtils.trimToEmpty(request.getParameter(PHRASE_PARAM));
+    }
+
+    public String getJobId() {
+        return StringUtils.trimToEmpty(request.getParameter(JOB_ID_PARAM));
+    }
+
+    public List<String> getBundleIds() {
+        String[] values = request.getParameterValues(BUNDLE_ID_PROP);
+        return ArrayUtils.isNotEmpty(values) ? Lists.newArrayList(values) : Collections.<String>emptyList();
+    }
+
+    public List<String> getBundleClasses() {
+        final String[] values = request.getParameterValues(BUNDLE_CLASS_PROP);
+
+        return ArrayUtils.isEmpty(values) ? Collections.<String>emptyList() : Lists.newArrayList(values);
+    }
+
+}
